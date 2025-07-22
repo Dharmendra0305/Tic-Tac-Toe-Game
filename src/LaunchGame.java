e25f58c1d678e32dfb1e89e8a0ee4df0a84a69cb
Dharmendra0305 <dharmendra.kp03@gmail.com>
2025-07-22 21:42:57 +0530

    update LaunchGame.java file

--- src/LaunchGame.java
+++ src/LaunchGame.java
@@ -1,27 +1,35 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class TicTacToe {
+class TicTacToe
+{
     static char[][] board;
 
-    public TicTacToe() {
+    public TicTacToe()
+    {
         board = new char[3][3];
         initBoard();
     }
 
-    void initBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
+    void initBoard()
+    {
+        for (int i = 0; i < board.length; i++)
+        {
+            for (int j = 0; j < board[i].length; j++)
+            {
                 board[i][j] = ' ';
             }
         }
     }
 
-    static void dispBoard() {
+    static void dispBoard()
+    {
         System.out.println("-------------");
-        for (int i = 0; i < board.length; i++) {
+        for (int i = 0; i < board.length; i++)
+        {
             System.out.print("| ");
-            for (int j = 0; j < board[i].length; j++) {
+            for (int j = 0; j < board[i].length; j++)
+            {
                 System.out.print(board[i][j] + " | ");
             }
             System.out.println();
@@ -29,41 +37,55 @@ class TicTacToe {
         }
     }
 
-    static void placeMark(int row, int col, char mark) {
-        if (row >= 0 && row <= 3 && col >= 0 && col <= 3) {
+    static void placeMark(int row, int col, char mark)
+    {
+        if (row >= 0 && row <= 3 && col >= 0 && col <= 3)
+        {
             board[row][col] = mark;
-        } else {
+        } else
+        {
             System.out.println("Invalid Position");
         }
     }
 
-    static boolean checkColWin() {
-        for (int j = 0; j <= 2; j++) {
-            if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+    static boolean checkColWin()
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            if (board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j])
+            {
                 return true;
             }
         }
         return false;
     }
 
-    static boolean checkRowWin() {
-        for (int i = 0; i <= 2; i++) {
-            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+    static boolean checkRowWin()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            if (board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+            {
                 return true;
             }
         }
         return false;
     }
 
-    static boolean checkDiagonalWin() {
+    static boolean checkDiagonalWin()
+    {
         return board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]
                 || board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0];
     }
 
-    static boolean checkDraw() {
-        for (int i = 0; i <= 2; i++) {
-            for (int j = 0; j <= 2; j++) {
-                if (board[i][j] == ' ') {
+    static boolean checkDraw()
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            for (int j = 0; j <= 2; j++)
+            {
+                if (board[i][j] == ' ')
+                {
                     return false;
                 }
             }
@@ -72,7 +94,8 @@ class TicTacToe {
     }
 }
 
-abstract class Player {
+abstract class Player
+{
     String name;
     char mark;
 
@@ -88,8 +111,10 @@ abstract class Player {
     }
 }
 
-class HumanPlayer extends Player {
-    HumanPlayer(String name, char mark) {
+class HumanPlayer extends Player
+{
+    HumanPlayer(String name, char mark)
+    {
         this.name = name;
         this.mark = mark;
     }
@@ -116,13 +141,16 @@ class HumanPlayer extends Player {
     }
 }
 
-class AIPlayer extends Player {
-    AIPlayer(String name, char mark) {
+class AIPlayer extends Player
+{
+    AIPlayer(String name, char mark)
+    {
         this.name = name;
         this.mark = mark;
     }
 
-    void makeMove() {
+    void makeMove()
+    {
         int row, col;
         do
         {
@@ -134,8 +162,10 @@ class AIPlayer extends Player {
     }
 }
 
-public class LaunchGame {
-    public static void main(String[] args) {
+public class LaunchGame
+{
+    public static void main(String[] args)
+    {
         new TicTacToe();
         HumanPlayer p1 = new HumanPlayer("Bob", 'X');
         AIPlayer p2 = new AIPlayer("Gemini", 'O');
@@ -143,21 +173,27 @@ public class LaunchGame {
         Player cp;
         cp = p1;
 
-        while (true) {
+        while (true)
+        {
             System.out.println(cp.name + " turn");
             cp.makeMove();
             TicTacToe.dispBoard();
 
-            if (TicTacToe.checkColWin() || TicTacToe.checkRowWin() || TicTacToe.checkDiagonalWin()) {
+            if (TicTacToe.checkColWin() || TicTacToe.checkRowWin() || TicTacToe.checkDiagonalWin())
+            {
                 System.out.println(cp.name + " has won");
                 break;
-            } else if (TicTacToe.checkDraw()) {
+            } else if (TicTacToe.checkDraw())
+            {
                 System.out.println("Game is a draw");
                 break;
-            } else {
-                if (cp == p1) {
+            } else
+            {
+                if (cp == p1)
+                {
                     cp = p2;
-                } else {
+                } else
+                {
                     cp = p1;
                 }
             }
